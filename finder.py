a5a8762f1459ef1597066f7d9baf40e48c1c1648
owain lewis <owain@owainlewis.com>
2013-01-07 22:45:24 +0000

    better checking of word lengths

--- finder.py
+++ finder.py
@@ -46,33 +46,36 @@ def make_full_domain(word):
 def word_starts_with(word, letter):
     return word[0] == letter
 
+# Check if a word is between upper and lower bound for length
+def word_is_between(word, lower, upper):
+    word_length = len(word)
+    return word_length >= lower and word_length <= upper
+
 # Save the search result to disk
 def save_result(domain, letter):
     file_name = "%s.txt" % letter
     with open(file_name, "a") as f:
         f.write(domain + '\n')
 
-def main(word_file, letter='a'):
-    try:
-      with open(word_file, 'r') as file:
-          for line in file:
-              if line[0] is letter:
-                  word = line.strip()
-                  if (len(word) > 4):
-                      domain = make_full_domain(word)
-                      try:
+def main(word_file, letter='a', lower_bound=4, upper_bound=10):
+    with open(word_file, 'r') as file:
+        for line in file:
+            if line[0] is letter:
+                word = line.strip()
+                if word_is_between(word, lower_bound, upper_bound):
+                    domain = make_full_domain(word)
+                    try:
                         response = parse_response(search(domain))
                         if response: 
                             print "Found domain %s" % (domain)
                             save_result(domain, letter)
-                      except: pass
-
-    except KeyboardInterrupt:
-        sys.exit(1)
+                    except: pass
 
 if __name__ == '__main__':
-    if len (sys.argv) == 3:
-        main(sys.argv[1], sys.argv[2])
+    if len (sys.argv) == 4:
+        lower = 4
+        upper = sys.argv[3]
+        main(sys.argv[1], sys.argv[2], lower, upper)
     else:
         sys.exit('Usage: python finder.py word_file letter')
 
